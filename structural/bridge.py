90cbc6d68e193a8dfffba7171aecb1c0b21d4219
amirali <167849011+amirali-khalili@users.noreply.github.com>
2025-11-13 16:05:18 +0330

    add Bridge

---
+++ structural/bridge.py
@@ -0,0 +1,45 @@
+from abc import ABC , abstractmethod
+
+
+# Implementation
+class Serve(ABC):
+
+    @abstractmethod
+    def serve(self):
+        pass
+
+class Cup(Serve):
+    def serve(self):
+        return 'cup serve..'
+    
+class Bottle(Serve):
+    def serve(self):
+        return 'Bottle serve..'    
+    
+class TakeAway(Serve):
+    def serve(self):
+        return 'TakeAway serve..'
+# Abstraction 
+class Panos(ABC):
+    def __init__(self,sarve:Serve):
+        self.sarve = sarve
+
+    @abstractmethod
+    def tea(self) :
+        pass
+
+
+class Coffee(Panos) :
+    def tea(self) ->int :
+        return f"cofe  serve : {self.sarve.serve()}"
+
+class Tea(Panos):
+    def tea(self):
+        return f"Tea  serve : {self.sarve.serve()}"
+
+class Smoothie(Panos):
+    def tea(self):
+        return f"Smoothie  serve : {self.sarve.serve()}"
+
+co = Coffee(Bottle())
+print(co.tea())
