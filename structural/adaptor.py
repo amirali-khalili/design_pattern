1ead0ac3695c04a7ad58d8d38db834c999bc497c
amirali <167849011+amirali-khalili@users.noreply.github.com>
2025-11-13 12:04:58 +0330

    add Adapter

---
+++ structural/adaptor.py
@@ -0,0 +1,21 @@
+from abc import ABC,abstractmethod
+
+
+class OlsSiste:
+    def make_transaction(self,value):
+        print(f"pardakht ghadimi {value}")
+
+class NewSistem(ABC):
+    @abstractmethod
+    def process_payment(self,amount):
+        pass
+
+class AdaptorOld(NewSistem):
+    def __init__(self,old_sistem:OlsSiste):
+        self.old_sistem=old_sistem
+    def process_payment(self,amount):
+        self.old_sistem.make_transaction(amount)
+
+a = AdaptorOld(OlsSiste())
+a.process_payment(200000)
+ 
