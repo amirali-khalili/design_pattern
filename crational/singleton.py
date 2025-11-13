35934e36d3f7c128cc188f6e0de190c2814bcf88
amirali <167849011+amirali-khalili@users.noreply.github.com>
2025-11-13 10:36:29 +0330

    edited

--- crational/singleton.py
+++ crational/singleton.py
@@ -1,16 +1,16 @@
 class SingletonMeta(type):
     def __call__(cls, *args, **kwargs):
-        print("hello")
+        # print("hello")
         return super().__call__(*args, **kwargs)
 
-class Singleton(metaclass=SingletonMeta):
+class Singleton1(metaclass=SingletonMeta):
     def __init__(self, name):
         self.name = name
 
-class child(Singleton):
+class child(Singleton1):
     pass
 
-s1 = Singleton("ali")
+s1 = Singleton1("ali")
 
 class mmm:
     def __call__(self):
@@ -21,14 +21,14 @@ class lll(mmm):
     def __init__(self, name):
         self.name = name
 
-class child(Singleton):
+class child(Singleton1):
     pass
 
 s1 = lll("ali")
 
 class Meta(type):
     def __new__(cls, name, bases, dct):
-        print("Meta.__new__ صدا زده شد")
+        # print("Meta.__new__ صدا زده شد")
         return super().__new__(cls, name, bases, dct)
 
 class MyClass(metaclass=Meta):
@@ -36,4 +36,28 @@ class MyClass(metaclass=Meta):
         print("MyClass.__init__ صدا زده شد")
 
 
-print(type(Singleton))
+# print(type(Singleton1))
+
+
+
+class Meta(type):
+    _instance ={}
+
+    def __call__(cls, *args, **kwds):
+        if cls not in cls._instance :
+            instance = super().__call__(*args, **kwds)
+            cls._instance[cls] = instance
+        return cls._instance[cls] 
+    
+class Singleton(metaclass = Meta):
+    pass
+
+class Test(Singleton):
+    def __init__(self,data):
+        self.data = data 
+
+
+a= Test([1,2,3])        
+print(a.data)
+b= Test([1,2,4])        
+print(a.data)
